fe854bcf6056c2f900307c9801b92a68c901d0a3
chlqhdgus1 <chlqhdgus1@naver.com>
2020-04-08 19:19:54 +0900

    blog페이지 만들기 입력창1

--- bitserver.js
+++ bitserver.js
@@ -1,73 +1,49 @@
 var http = require("http");
 var express = require("express");
 
-var app = express();            //express객체 생성
-app.use(express.static("public"));
-app.use(express.bodyParser());      //사용자로부터 파라메타를 받아드려 처리 할수 있음
-app.use(app.router);            //서버 사용자마다 다르게 하기 위해 router
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'bit';
 
+var app = express();
+app.use(express.static("public"));
+app.use(express.bodyParser());
+app.use(app.router);
 
 app.get("/blogList", function(request, response){
-
   const client = new MongoClient(url);
   client.connect(function(err, client) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
-
     const db = client.db(dbName);
-
     const col = db.collection('articles');
+        col.find({}).toArray(function(err, docs) {
+        client.close();
+        response.send(docs);
+      });
+  });
+});
 
-    col.find({}).toArray(function(err, docs) {
 
+app.get("/blogInsert", function(request, respone){
+  var title = request.param("title");
+  var content = request.param("content");
+  var saved_at = new Date();
+  var doc = {title:title, content:content, saved_at:saved_at};
+
+    const client = new MongoClient(url);
+    client.connect(function(err, client) {
+    const db = client.db(dbName);
+    db.collection('articles').insertOne(doc, function(err, r) {
+      _id = r.insertedId;
       client.close();
-      response.send(docs);
+      respone.send(_id);
     });
   });
-
-})
-
-
-app.all("/insertblog",function(request, response){
-      var title = request.param("title");
-      var content = request.param("content");
-
-      var doc = {title:title,content:content}
-
-
-      const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-// Database name
-const dbName = 'bit';
-
-const client = new MongoClient(url);
-
-(async function() {
-  try {
-    await client.connect();
-    console.log("Connected correctly to server");
-    const db = client.db(dbName);
-
-    // Insert a single document
-    let r = await db.collection('articles').insertOne(doc);
-
-    // Close connection
-
-  } catch(err) {
-    console.log(err.stack);
-  }
-    client.close();
-})();
-})
-
-
-
-
+});
 
 
 http.createServer(app).listen(52273, function(){
-    console.log("서버가 가동되었습니다.");
-})
+  console.log("서버가 가동되었습니다");
+});
